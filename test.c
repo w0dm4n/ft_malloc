c5fe3d596acbc65eaa7e9d556460298d14d00c8a
Frederick MARINHO <frmarinh@e2r4p7.42.fr>
2017-02-20 13:47:25 +0100

    Main test

--- test.c
+++ test.c
@@ -15,15 +15,50 @@
 
 int main(int argc, char **argv)
 {
+	/*char *bjr = malloc(42);
+	char *cc = malloc(666);
+	free (bjr);
+	bjr = malloc(123);
+	free(cc);
+	free(bjr);*/
+	//free(bjr);
+	/*char *slt = malloc(42);
+	slt[0] = 'a';
+	printf("%s\n", slt);
+	free(slt);
+	slt = malloc(42);
+	slt[0] = 'a';
+	printf("%s\n", slt);
+	int i = 0;
+	while (i < 1000)
+	{
+		slt = malloc(42);
+		slt[0] = 'a';
+		printf("%s\n", slt);
+		free(slt);
+		i++;
+	}*/
+	//char *bjr = malloc(1025);
+	//free(bjr);
+	/*int i = 0;
+	while (i < 1000)
+	{
+		malloc(1000);
+		malloc(42);
+		malloc(100000);
+		//printf("%d\n", i);
+		i++;
+	}*/
+	int i = 0;
 	char *test = malloc(5);
 	char *test1 = malloc(10);
 	show_alloc_mem();
-	/*char *bonjour;
+	char *bonjour;
 
 	bonjour = malloc(5);
 	strcpy(bonjour, "test");
 	printf("Before reallocation: %s, adress: %p\n", bonjour, bonjour);
-	bonjour = realloc(bonjour, 10);
+	//bonjour = realloc(bonjour, 10);
 	printf("After reallocation: %s, adress: %p\n", bonjour, bonjour);
 	strcat(bonjour, "coucou");
 	printf("Modification: %s\n", bonjour);
@@ -44,7 +79,7 @@ int main(int argc, char **argv)
 	char *str5 = malloc(556);
 	total += 556;
 	
-	int i = -1;
+	i = -1;
 
 	printf("\n\n--- debugging ---\n\n");
 	while (++i < 42)
@@ -79,16 +114,17 @@ int main(int argc, char **argv)
 	}
 
 	printf("end\n");
-	return (0);
+	//return (0);
 
 	char *coucou = malloc(1024);
-	char *coucou1 = malloc(40);
+	char *coucou1 = malloc(4000000);
 	char *coucou2 = malloc(200000);
 	char *bjr = NULL;
 	char *bjr2 = NULL;
 	//char *coucou1 = malloc(40);
 	coucou[0] = 'a';
 	free(coucou);
+	free(coucou1);
 	//malloc(40);
 	//free(coucou1);
 	free(coucou2);
@@ -103,8 +139,8 @@ int main(int argc, char **argv)
 		free(bjr);
 		free(bjr2);
 		i++;
-	}*/
-	/*int i = 0;
+	}
+	i = 0;
 	while (i <= 100)
 	{
 		malloc(40);
@@ -134,7 +170,7 @@ int main(int argc, char **argv)
 	{
 		malloc(10024);
 		i++;
-	}*/
+	}
 
 	/*while (i < 666)
 		coucou1[i++] = 'a';
@@ -142,5 +178,5 @@ int main(int argc, char **argv)
 	printf("CONTENT: %s, SIZE: %lu, ADRESS: %p, PAGE_SIZE: %d", coucou1, sizeof(coucou1), coucou1, 
 		SMALL_ALLOC);
 	*/
-	//show_alloc_mem();
+	show_alloc_mem();
 }
